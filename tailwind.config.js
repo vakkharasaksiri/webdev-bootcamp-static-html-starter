049ce36bd49d7c3694ceeb222de9aee06585295d
Vashirawit <vakkharasaksiri@hotmail.com>
2023-12-23 13:46:21 +0700

    init setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,11 @@ module.exports = {
       },
       backgroundImage: {
         "hero-pattern":
-          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('/assets/hero/hero.jpg')",
+          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('../assets/hero/hero.jpg')",
       },
+      fontFamily:{
+        'sans':["poppins","Arial","sans-serif"]
+      }
     },
   },
   plugins: [],
